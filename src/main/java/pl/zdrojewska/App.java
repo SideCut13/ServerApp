3aae633808fbade0e8cb55089242294b25a4d265
Anna Zdrojewska <anna_zdrojewska@interia.eu>
2018-06-16 10:51:51 +0200

    edit connection login with Client

--- src/main/java/pl/zdrojewska/App.java
+++ src/main/java/pl/zdrojewska/App.java
@@ -3,6 +3,7 @@ package pl.zdrojewska;
 import pl.zdrojewska.service.Service;
 import pl.zdrojewska.service.ServiceImpl;
 import pl.zdrojewska.tables.Client;
+import pl.zdrojewska.tables.Meal;
 
 import java.io.*;
 import java.net.ServerSocket;
@@ -20,10 +21,10 @@ public class App {
 
     public static void main(String[] args)
     {
-        Service service = new ServiceImpl();
-        service.addMealService("Sniadanie","Bulka z serem", "Posmarowac bulke maslem,dodac ser", "12341234", "", 1L);
+        //Service service = new ServiceImpl();
+       // service.addMealService("Sniadanie","Bulka z serem", "Posmarowac bulke maslem,dodac ser", "12341234", "", 1L);
 
-       /* try {
+        try {
             ExecutorService executor = Executors.newCachedThreadPool();
 
 
@@ -43,7 +44,7 @@ public class App {
         {
             System.out.print("Nie udalo sie utworzyc polaczenia");
             System.out.print(e.getMessage());
-        }*/
+        }
 
 
     }
@@ -55,6 +56,7 @@ class ClientConnection implements Runnable
     private Socket clientSocket;
     private PrintWriter writer;
     private BufferedReader bufferedReader;
+    private Service service = new ServiceImpl();
 
     public ClientConnection(Socket clientSocket) throws IOException {
 
@@ -86,61 +88,78 @@ class ClientConnection implements Runnable
         }
     }
 
-    public void login(String login,String password) throws IOException
-    {
-        if(login.equals("Antoni") && password.equals("Dzik"))
-        {
-            writer.println("OK");
-
-            String status = bufferedReader.readLine();
-            if(status.equals("Sync"))
-            {
-                List idsList = new ArrayList<Integer>();
+    public void login(String login,String password) throws IOException {
+        List<Client> clients = service.getAllClientsService();
+        Long idClient = null;
+        for (int i = 0; i < clients.size(); i++) {
+            if (clients.get(i).getLogin().equals(login) && clients.get(i).getPassword().equals(password)) {
                 writer.println("OK");
-                status = bufferedReader.readLine();
-                if(status.equals("Send")) {
-                    String readId = bufferedReader.readLine();
-                    while (!readId.equals("Endsend")) {
-                        idsList.add(Integer.parseInt(readId));
-                        readId = bufferedReader.readLine();
-                    }
-                    writer.println("OK");
-                    writer.println("Send");
-
-                    //sending first meal
-                    writer.println("2");    //id
-                    writer.println("Sniadanie"); //title
-                    writer.println("Bulka z maslem"); //summary
-                    writer.println("Posmarowac bulke maslem"); //description
-                    writer.println("12323234"); // date
-                    writer.println(""); //image path
-
-                    //end sending first meal
-                    writer.println("isNext");
-                    //sending second meal
-
-                    writer.println("3");    //id
-                    writer.println("Sniadanie"); //title
-                    writer.println("Bulka z serem"); //summary
-                    writer.println("Posmarowac bulke maslem,dodac ser"); //description
-                    writer.println("123234234"); // date
-                    writer.println(""); //image path
-
-                    //end sending second meal
-
-                    //end sending meals
-
-                    writer.println("Endsend");
-                }
-                else
-                {
-                    status = null;
-                }
-
+                idClient = Long.valueOf(i);
             }
-        }else
-        {
+        }
+        String status = bufferedReader.readLine();
+        if (status.equals("Sync")) {
+            List<Long> idsList = new ArrayList<>();
+            writer.println("OK");
+            status = bufferedReader.readLine();
+            if (status.equals("Send")) {
+                String readId = bufferedReader.readLine();
+                while (!readId.equals("Endsend")) {
+                    idsList.add(Long.parseLong(readId));
+                    readId = bufferedReader.readLine();
+                }
+                writer.println("OK");
+                writer.println("Send");
+                //sending first meal
+                List<Meal> meals = service.getAllMealsService();
+
+                for (int i = 0; i <meals.size() ; i++) {
+                    if(meals.get(i).getClient().equals(idClient)){
+                        writer.println(meals.get(i).getId());
+                        writer.println(meals.get(i).getTitle());
+                        writer.println(meals.get(i).getSummary());
+                        writer.println(meals.get(i).getDescription());
+                        writer.println(meals.get(i).getDate());
+                        writer.println(meals.get(i).getImagePath());
+                        writer.println("isNext");
+                    }
+                }
+                writer.println("Endsend");
+
+            } else {
+                status = null;
+            }
+
+        } else {
             writer.println("NOK");
         }
+
+               /* writer.println("2");    //id
+                writer.println("Sniadanie"); //title
+                writer.println("Bulka z maslem"); //summary
+                writer.println("Posmarowac bulke maslem"); //description
+                writer.println("12323234"); // date
+                writer.println(""); //image path
+
+                //end sending first meal
+                writer.println("isNext");
+                //sending second meal
+
+                writer.println("3");    //id
+                writer.println("Sniadanie"); //title
+                writer.println("Bulka z serem"); //summary
+                writer.println("Posmarowac bulke maslem,dodac ser"); //description
+                writer.println("123234234"); // date
+                writer.println(""); //image path
+
+                //end sending second meal
+
+                //end sending meals
+
+                writer.println("Endsend");*/
+
     }
+
+
+
 }
